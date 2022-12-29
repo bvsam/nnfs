b88f8f854a6542eb3221aa2643b7d126a5670e6c
Benjamin Sam <79420331+bsam04@users.noreply.github.com>
2022-12-29 16:05:14 -0500

    Created a basic optimizer and tested the model with 10000 epochs

--- neuralNet.py
+++ neuralNet.py
@@ -175,48 +175,67 @@ class ActivationSoftmax_LossCategoricalCrossEntropy:
         self.dinputs /= samples
 
 
+class OptimizerSGD:
+    def __init__(self, learningRate=1.0):
+        # Set the learning rate
+        self.learningRate = learningRate
+
+    def updateParams(self, layer):
+        # Update the layer's weights and biases by multiplying the learning rate by the gradients of the weights and biases
+        # Subtraction is used here because a positive gradient indicates a direction of ascent, and we want to move in the opposite direction
+        layer.weights -= self.learningRate * layer.dweights
+        layer.biases -= self.learningRate * layer.dbiases
+
+
 # Create a spiral dataset with 100 points and 3 classes. The size of the dataset is 300 x 2, and the size of the labels is 300 x 1.
 X, y = spiral_data(100, 3)
 
-# Create a dense layer with 2 input features and 3 output values
-dense1 = LayerDense(2, 3)
+# Create a dense layer with 2 input features and 64 output values
+dense1 = LayerDense(2, 64)
 # Create a ReLU activation (to be used with the first dense layer):
 activation1 = ActivationReLU()
 
 # Create a second dense layer with 3 input features (as we take the output of the first layer here) and 3 output values (for the 3 classes of the spiral dataset)
-dense2 = LayerDense(3, 3)
+dense2 = LayerDense(64, 3)
 # Create a combined activation and loss function object
 lossActivation = ActivationSoftmax_LossCategoricalCrossEntropy()
 
-# Perform a forward pass of our training data through this layer
-dense1.forward(X)
-# Perform a forward pass through activation function in the first layer
-activation1.forward(dense1.output)
-# Perform a forward pass through the second dense layer
-dense2.forward(activation1.output)
-# Perform a forward pass through the combined activation and loss method, and store the calculated loss
-loss = lossActivation.forward(dense2.output, y)
+# Create an optimizer object
+optimizer = OptimizerSGD()
 
-# Print the first few values of the output of the network, along with the calculated loss
-print(lossActivation.output[:5])
-print(f"Loss: {loss}")
+for epoch in range(10001):
+    # Perform a forward pass of our training data through this layer
+    dense1.forward(X)
+    # Perform a forward pass through activation function in the first layer
+    activation1.forward(dense1.output)
+    # Perform a forward pass through the second dense layer
+    dense2.forward(activation1.output)
+    # Perform a forward pass through the combined activation and loss method, then store the calculated loss
+    loss = lossActivation.forward(dense2.output, y)
 
-# Calculate the model's accuracy and print it
-predictions = np.argmax(lossActivation.output, axis=1)
-if len(y.shape) == 2:
-    y = np.argmax(y, axis=1)
-accuracy = np.mean(predictions == y)
-print(f"Accuracy: {accuracy}")
+    # Calculate the model's accuracy
+    predictions = np.argmax(lossActivation.output, axis=1)
+    if len(y.shape) == 2:
+        y = np.argmax(y, axis=1)
+    accuracy = np.mean(predictions == y)
 
-# Perform a backward pass through the combined activation and loss method
-lossActivation.backward(lossActivation.output, y)
-# Perform a backward pass through the rest of the dense layers and activation functions
-dense2.backward(lossActivation.dinputs)
-activation1.backward(dense2.dinputs)
-dense1.backward(activation1.dinputs)
+    # Print the epoch number, accuracy, and loss every 100 epochs
+    if not epoch % 100:
+        print(
+            f"""
+        Epoch: {epoch}
+        Accuracy: {accuracy:.3f}
+        Loss: {loss:.3f}
+        """
+        )
 
-# Print the calculated gradients
-print(dense1.dweights)
-print(dense1.dbiases)
-print(dense2.dweights)
-print(dense2.dbiases)
+    # Perform a backward pass through the combined activation and loss method
+    lossActivation.backward(lossActivation.output, y)
+    # Perform a backward pass through the rest of the dense layers and activation functions
+    dense2.backward(lossActivation.dinputs)
+    activation1.backward(dense2.dinputs)
+    dense1.backward(activation1.dinputs)
+
+    # Update the weights and biases using the optimizer
+    optimizer.updateParams(dense1)
+    optimizer.updateParams(dense2)
